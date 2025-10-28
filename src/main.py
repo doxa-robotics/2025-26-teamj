978f9d5d0c2ec5426fde4f39815a3f9d0bef6d05
Jinwoong0327 <234563524+Jinwoong0327@users.noreply.github.com>
2025-10-28 17:16:27 +0900

    changed comments

--- src/main.py
+++ src/main.py
@@ -17,15 +17,12 @@ def autonomous():
     brain.screen.print("autonomous code")
     # place automonous code here
 
-def user_control():
+def driver_control():
     brain.screen.clear_screen()
     brain.screen.print("driver control")
     # place driver control in this while loop
     while True:
         wait(20, MSEC)
 
-# create competition instance
-comp = Competition(user_control, autonomous)
-
-# actions to do when the program starts
-brain.screen.clear_screen()
+# Tell VEX what *functions* we want to run when
+Competition(driver_control, autonomous)
