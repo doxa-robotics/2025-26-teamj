eabfdb724d6497f2722242e1f49dd71d2a946e72
Jinwoong0327 <234563524+Jinwoong0327@users.noreply.github.com>
2025-11-13 17:19:03 +0900

    fixing error on intake

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ motor_right_3 = Motor(Ports.PORT5, False)
 #match_loa 1 is extend and 2 is retract
 left_motors = MotorGroup(motor_left_1, motor_left_2, motor_left_3)
 right_motors = MotorGroup(motor_right_1, motor_right_2, motor_right_3)
-motor_intake = Motor(Ports.PORT6, False)
+motor_intake = Motor(Ports.PORT6, True)
 motor_intake_2 = Motor(Ports.PORT7, False)
 match_load = Pneumatics(brain.three_wire_port.b)
 
